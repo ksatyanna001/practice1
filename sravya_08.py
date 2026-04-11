796d01a58a0f7c2ec0762e98efa213241254e327
yeligate-sravya <sravyayeligate@gmail.com>
2026-04-11 19:21:26 +0530

    added file

---
+++ sravya_08.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+
+print("Hello,", name, "! Welcome to Python.")
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+sum_result = num1 + num2
+
+print("The sum is:", sum_result)
